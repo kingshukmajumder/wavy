49bb156dca4ddfc35888b492bb5fd596dda1e8f9
kingshukmajumder <kingshuk1000@gmail.com>
2023-11-19 14:56:32 +0530

    Support for names.

--- state.js
+++ state.js
@@ -1,3 +1,14 @@
+class Waveform {
+    constructor(name, values) {
+        this.name = name;
+        this.values = values;
+    }
+}
 class State {
-
+    constructor() {
+        this.waves = []
+    }
+    addWaveform(waveform) {
+        this.waves.push(waveform);
+    }
 }
--- viewer.js
+++ viewer.js
@@ -1,6 +1,27 @@
 function onScroll(obj, id) {
     e = document.getElementById(id)
-    e.style.top = `${400 - obj.scrollTop}px`
+    e.style.top = `${-obj.scrollTop}px`
+}
+
+function drawNameWindow(id, state) {
+    const wndw = document.getElementById(id);
+    for (let i = 0; i < state.waves.length; i++) {
+        wave = state.waves[i];
+        lbl = document.createElement('label')
+        lbl.innerHTML = wave.name
+        lbl.style = `position: absolute; top: ${10 + 50 * i}px; left: 10px;`
+        wndw.appendChild(lbl)
+    }
+}
+
+function drawWaveformWindow(id, state) {
+    const c = document.getElementById(id);
+    const ctx = c.getContext("2d");
+
+    for (let i = 0; i < state.waves.length; i++) {
+        wave = state.waves[i];
+        drawBinaryWave(ctx, 0, 30 + i * 50, wave.values)
+    }
 }
 
 function drawBinaryWave(ctx, x, y, values) {
@@ -20,20 +41,18 @@ function drawBinaryWave(ctx, x, y, values) {
         ctx.lineTo(x, y);
         prev = val
     }
+    ctx.strokeStyle = '#2eb82e'
     ctx.stroke();
 }
 
-
-
-function drawWaveformWindow(id) {
-    const c = document.getElementById(id);
-    const ctx = c.getContext("2d");
-    values = []
-    for (let i = 0; i < 100; i++) {
-        values.push(i % 2)
-    }
-    for (let i = 0; i < 15; i++) {
-        drawBinaryWave(ctx, 0, 20 + i * 50, values)
-    }
+function loadWaveform(wave_wndw_id, name_wndw_id, state) {
+    state.addWaveform(new Waveform("wave1", [1, 0, 1, 0, 1, 0]))
+    state.addWaveform(new Waveform("wave2", [1, 0, 1, 1, 1, 0]))
+    state.addWaveform(new Waveform("wave3", [0, 1, 1, 0, 1, 0]))
+    state.addWaveform(new Waveform("wave4", [0, 1, 1, 0, 1, 1]))
+    drawWaveformWindow(wave_wndw_id, state)
+    drawNameWindow(name_wndw_id, state)
 }
+
+
 
